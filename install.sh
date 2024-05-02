d1146f51a728495cc92fd18c83f1f379e1b6d21c
esteves <nuno.pinto@live.com>
2024-05-02 21:47:21 +0100

    added tmux config

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 
-
 sudo pacman -Sy --needed zsh stow tmux fzf ripgrep fd go rustup neovim gcc xclip nodejs npm
 
 stow .
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 rustup default stable
