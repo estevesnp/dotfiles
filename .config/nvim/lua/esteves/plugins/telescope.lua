02908c0389b514c4f4ee090a5f9a76814a6dc8db
estevesnp <nuno.pinto@live.com>
2024-05-03 01:23:59 +0100

    added functionalities to telescope

--- .config/nvim/lua/esteves/plugins/telescope.lua
+++ .config/nvim/lua/esteves/plugins/telescope.lua
@@ -1,13 +1,28 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.6',
-	-- or                              , branch = '0.1.x',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-		vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
-		vim.keymap.set('n', '<leader>sb', builtin.buffers, {})
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+		vim.keymap.set("n", "<leader>gf", builtin.git_files, { desc = "Search [G]it [F]iles" })
+		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch [G]rep" })
+		vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[S]earch [B]uffers" })
+		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 
-	end
+		vim.keymap.set("n", "<leader>/", function()
+			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+				winblend = 10,
+				previewer = false,
+			}))
+		end, { desc = "[/] Fuzzily search in current buffer" })
+
+		vim.keymap.set("n", "<leader>sn", function()
+			builtin.find_files({ cwd = vim.fn.stdpath("config") })
+		end, { desc = "[S]earch [N]eovim files" })
+	end,
 }
