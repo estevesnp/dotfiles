cb3f1e8bb1bfa50d4841e179c4df571d435c8b5f
esteves <nuno.pinto@live.com>
2024-05-05 18:12:15 +0100

    added bind for restarting lsp

--- .config/nvim/lua/esteves/keymaps.lua
+++ .config/nvim/lua/esteves/keymaps.lua
@@ -1,8 +1,13 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>rw", vim.cmd.Ex, { desc = "Open Net[R][W]" })
 
+-- Simple bindings
+vim.keymap.set("n", "<leader>rw", vim.cmd.Ex, { desc = "Open Net[R][W]" })
+vim.keymap.set("n", "<leader>rl", "<cmd>LspRestart<CR>", { desc = "[R]estart [L]SP" })
+
+-- Makes <C-c> work as <Esc> in insert mode
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
+-- Remove search highlights after pressing <Esc>
 vim.opt.hlsearch = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
@@ -47,9 +52,3 @@ vim.keymap.set("n", "<leader>pd", vim.diagnostic.goto_prev, { desc = "Go to [P]r
 vim.keymap.set("n", "<leader>nd", vim.diagnostic.goto_next, { desc = "Go to [N]ext [D]iagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
-
--- Split navigation keymaps
---vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
---vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
---vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
---vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
