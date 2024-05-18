0b797c7ec0c79b94b239f823897bf3000b33d687
estevesnp <nuno.pinto@live.com>
2024-05-18 01:01:01 +0100

    show hidden by default, disable ctrl+c from killing oil

--- .config/nvim/lua/esteves/plugins/oil.lua
+++ .config/nvim/lua/esteves/plugins/oil.lua
@@ -2,7 +2,14 @@ return {
 	"stevearc/oil.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require("oil").setup()
+		require("oil").setup({
+			keymaps = {
+				["<C-c>"] = false,
+			},
+			view_options = {
+				show_hidden = true,
+			},
+		})
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	end,
 }
