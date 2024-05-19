e7bda9196802b3bc40ecb165542497f91544c56f
estevesnp <nuno.pinto@live.com>
2024-05-19 19:21:04 +0100

    updated dependencies comments

--- .config/nvim/lua/esteves/plugins/dap-go.lua
+++ .config/nvim/lua/esteves/plugins/dap-go.lua
@@ -1,7 +1,17 @@
 return {
 	"leoluz/nvim-dap-go",
-	-- requires delve: go install github.com/go-delve/delve/cmd/dlv@latest
+	-- requires delve:
+	-- go install github.com/go-delve/delve/cmd/dlv@latest
 	dependencies = "mfussenegger/nvim-dap",
+	build = function()
+		-- Run the go install command
+		local result = vim.fn.system("go install github.com/go-delve/delve/cmd/dlv@latest")
+		if vim.v.shell_error ~= 0 then
+			vim.notify("Failed to install delve: " .. result, vim.log.levels.ERROR)
+		else
+			vim.notify("Successfully installed delve")
+		end
+	end,
 	ft = "go",
 	config = function()
 		require("dap-go").setup()
