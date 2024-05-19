07b040cd45b55640276ef33b8d59e547ca2c3992
estevesnp <nuno.pinto@live.com>
2024-05-19 12:42:34 +0100

    added oil bindings

--- .config/nvim/lua/esteves/plugins/oil.lua
+++ .config/nvim/lua/esteves/plugins/oil.lua
@@ -5,11 +5,18 @@ return {
 		require("oil").setup({
 			keymaps = {
 				["<C-c>"] = false,
+				["<C-l>"] = false,
+				["<C-h>"] = false,
+				["<M-l>"] = "actions.refresh",
+				["<M-h>"] = "actions.select_split",
+				["<M-v>"] = "actions.select_vsplit",
+				["q"] = "actions.close",
 			},
 			view_options = {
 				show_hidden = true,
 			},
 		})
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		vim.keymap.set("n", "<leader>-", require("oil").toggle_float, { desc = "Toggle oil float" })
 	end,
 }
