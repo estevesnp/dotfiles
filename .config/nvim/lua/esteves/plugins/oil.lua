44c6abceb8e9af18862ba67e77e6bac4d938e94c
estevesnp <nuno.pinto@live.com>
2024-06-14 14:12:48 +0100

    added oil.nvim configs

--- .config/nvim/lua/esteves/plugins/oil.lua
+++ .config/nvim/lua/esteves/plugins/oil.lua
@@ -3,6 +3,14 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("oil").setup({
+			default_file_explorer = true,
+			skip_confirm_for_simple_edits = true,
+			view_options = {
+				show_hidden = true,
+				is_always_hidden = function(name, _)
+					return name == ".." or name == ".git"
+				end,
+			},
 			keymaps = {
 				["<C-c>"] = false,
 				["<C-l>"] = false,
@@ -12,9 +20,6 @@ return {
 				["<M-v>"] = "actions.select_vsplit",
 				["<C-s>"] = ":w<CR>",
 				["q"] = "actions.close",
-			},
-			view_options = {
-				show_hidden = true,
 			},
 		})
 		local map = CreateNamedMap("Oil")
