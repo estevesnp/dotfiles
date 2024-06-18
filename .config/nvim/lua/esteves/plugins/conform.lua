7c91f908837e79ea156d4e843a3803980687b47d
estevesnp <nuno.pinto@live.com>
2024-06-18 18:48:11 +0100

    added option to disable format on save, adjusted lua indentation

--- .config/nvim/lua/esteves/plugins/conform.lua
+++ .config/nvim/lua/esteves/plugins/conform.lua
@@ -1,20 +1,15 @@
 return {
-	{ -- Autoformat
-		"stevearc/conform.nvim",
-		lazy = false,
-		keys = {
-			{
-				"<leader>f",
-				function()
-					require("conform").format({ async = true, lsp_fallback = true })
-				end,
-				mode = "",
-				desc = "[F]ormat buffer",
-			},
-		},
-		opts = {
+	"stevearc/conform.nvim",
+	lazy = false,
+	config = function()
+		require("conform").setup({
 			notify_on_error = false,
 			format_on_save = function(bufnr)
+				-- For FormatDisable and FormatEnable commands
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					---@diagnostic disable-next-line: missing-return-value
+					return
+				end
 				-- Disable "format_on_save lsp_fallback" for languages that don't
 				-- have a well standardized coding style.
 				local disable_filetypes = { c = true, cpp = true }
@@ -27,6 +22,29 @@ return {
 				lua = { "stylua" },
 				go = { "goimports", "gofumpt" },
 			},
-		},
-	},
+		})
+
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				-- FormatDisable! will disable formatting just for this buffer
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true,
+		})
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
+		})
+
+		local map = CreateNamedMap("Conform")
+		map("n", "<leader>f", function()
+			require("conform").format({ async = true, lsp_fallback = true })
+		end, "[F]ormat bufer")
+	end,
 }
