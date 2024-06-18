443502c661e53d754bb0bc61267deab5d2ca7498
estevesnp <nuno.pinto@live.com>
2024-06-18 01:18:24 +0100

    added more snippets

--- .config/nvim/lua/esteves/plugins/cmp.lua
+++ .config/nvim/lua/esteves/plugins/cmp.lua
@@ -18,12 +18,12 @@ return {
 					-- `friendly-snippets` contains a variety of premade snippets.
 					--    See the README about individual language/framework/plugin snippets:
 					--    https://github.com/rafamadriz/friendly-snippets
-					-- {
-					--   'rafamadriz/friendly-snippets',
-					--   config = function()
-					--     require('luasnip.loaders.from_vscode').lazy_load()
-					--   end,
-					-- },
+					{
+						"rafamadriz/friendly-snippets",
+						config = function()
+							require("luasnip.loaders.from_vscode").lazy_load()
+						end,
+					},
 				},
 			},
 			"saadparwaiz1/cmp_luasnip",
@@ -50,7 +50,7 @@ return {
 					-- Select the [p]revious item
 					["<C-p>"] = cmp.mapping.select_prev_item(),
 
-					-- Scroll the documentation window [b]ack / [f]orward
+					-- Scroll the documentation window [b]ack / [e]orward
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-e>"] = cmp.mapping.scroll_docs(4),
 
@@ -65,12 +65,6 @@ return {
 					["<C-Space>"] = cmp.mapping.complete({}),
 
 					-- Think of <c-l> as moving to the right of your snippet expansion.
-					--  So if you have a snippet that's like:
-					--  function $name($args)
-					--    $body
-					--  end
-					--
-					-- <c-l> will move you to the right of each of the expansion locations.
 					-- <c-h> is similar, except moving you backwards.
 					["<C-l>"] = cmp.mapping(function()
 						if luasnip.expand_or_locally_jumpable() then
