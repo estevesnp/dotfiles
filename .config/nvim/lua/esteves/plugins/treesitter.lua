b98ac98b95b51c1c3e6ef54ba5a79123f537c04a
estevesnp <nuno.pinto@live.com>
2024-06-20 15:39:35 +0100

    added vim and vimdoc to treesiter ensure installed

--- .config/nvim/lua/esteves/plugins/treesitter.lua
+++ .config/nvim/lua/esteves/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		opts = {
-			ensure_installed = { "go", "rust", "c", "java", "javascript", "typescript", "html", "lua" },
+			ensure_installed = { "go", "rust", "c", "java", "javascript", "typescript", "html", "lua", "vim", "vimdoc" },
 			auto_install = true,
 			highlight = {
 				enable = true,
