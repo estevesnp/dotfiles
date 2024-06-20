415d284f81401039f6a394a9ac8c91bbb75c9b47
estevesnp <nuno.pinto@live.com>
2024-06-20 01:21:24 +0100

    made plugin setups more similar to each other

--- .config/nvim/lua/esteves/plugins/harpoon.lua
+++ .config/nvim/lua/esteves/plugins/harpoon.lua
@@ -1,39 +1,30 @@
 return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
-	opts = {
-		settings = {
-			save_on_toggle = true,
-		},
-	},
-	keys = function()
-		local keys = {
-			{
-				"<leader>H",
-				function()
-					require("harpoon"):list():add()
-				end,
-				desc = "Harpoon: Add File",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local harpoon = require("harpoon")
+
+		harpoon:setup({
+			settings = {
+				save_on_toggle = true,
 			},
-			{
-				"<leader>h",
-				function()
-					local harpoon = require("harpoon")
-					harpoon.ui:toggle_quick_menu(harpoon:list())
-				end,
-				desc = "Harpoon: Toggle Menu",
-			},
-		}
+		})
+
+		local map = CreateNamedMap("Harpoon")
+
+		map("n", "<leader>h", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, "Toggle Harpoon Menu")
+
+		map("n", "<leader>H", function()
+			harpoon:list():add()
+		end, "Add File to Harpoon")
 
 		for i = 1, 5 do
-			table.insert(keys, {
-				"<leader>" .. i,
-				function()
-					require("harpoon"):list():select(i)
-				end,
-				desc = "Harpoon: Switch to File " .. i,
-			})
+			map("n", "<leader>" .. i, function()
+				harpoon:list():select(i)
+			end, "Switch to Harpoon File " .. i)
 		end
-		return keys
 	end,
 }
--- .config/nvim/lua/esteves/plugins/nvim-go.lua
+++ .config/nvim/lua/esteves/plugins/nvim-go.lua
@@ -16,11 +16,11 @@ return {
 		})
 		local map = CreateNamedMap("Go")
 
-		map("n", "<leader>ife", "<cmd> GoIfErr<CR>", "Add if err check")
-		map("n", "<leader>gtj", "<cmd> GoAddTags json<CR>", "Add json struct tags")
-		map("n", "<leader>grj", "<cmd> GoRemoveTags json<CR>", "Remove json struct tags")
-		map("n", "<leader>gty", "<cmd> GoAddTags yaml<CR>", "Add yaml struct tags")
-		map("n", "<leader>gry", "<cmd> GoRemoveTags yaml<CR>", "Remove yaml struct tags")
-		map("n", "<leader>gtc", "<cmd> GoClearTags<CR>", "Clear struct tags")
+		map("n", "<leader>ife", "<cmd>GoIfErr<CR>", "Add if err check")
+		map("n", "<leader>gtj", "<cmd>GoAddTags json<CR>", "Add json struct tags")
+		map("n", "<leader>grj", "<cmd>GoRemoveTags json<CR>", "Remove json struct tags")
+		map("n", "<leader>gty", "<cmd>GoAddTags yaml<CR>", "Add yaml struct tags")
+		map("n", "<leader>gry", "<cmd>GoRemoveTags yaml<CR>", "Remove yaml struct tags")
+		map("n", "<leader>gtc", "<cmd>GoClearTags<CR>", "Clear struct tags")
 	end,
 }
