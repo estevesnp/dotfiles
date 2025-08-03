7e45c652aeba8511eb5f3af108b41ca3e07d397d
estevesnp <nuno.pinto@live.com>
2025-08-04 00:15:54 +0100

    feat: macos script improvement

--- macos.sh
+++ macos.sh
@@ -2,14 +2,10 @@
 
 GHOSTTY_MAC_FILE='.config/ghostty/macos'
 
-if [ -e "$GHOSTTY_MAC_FILE" ]; then
-  echo "skipping $GHOSTTY_MAC_FILE"
-else
-  echo "creating $GHOSTTY_MAC_FILE"
+cat > "$GHOSTTY_MAC_FILE" <<EOF
+# macos only config
 
-  cat > "$GHOSTTY_MAC_FILE" <<EOF
 font-size = 14
+macos-option-as-alt = true
 
 EOF
-
-fi
