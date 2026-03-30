441ac7d20ca8c25e9c5ed07a736fc95642c9ba49
estevesnp <nuno.pinto@live.com>
2026-03-30 21:29:48 +0100

    chore: formatting

--- .dotsnip/build.zig
+++ .dotsnip/build.zig
@@ -13,7 +13,7 @@ pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "<your exe name here>",
         .root_module = mod,
-        .use_llvm = b.option(bool, "llvm", "use llvm for executable"),
+        .use_llvm = b.option(bool, "llvm", "Use LLVM for executable"),
     });
 
     b.installArtifact(exe);
